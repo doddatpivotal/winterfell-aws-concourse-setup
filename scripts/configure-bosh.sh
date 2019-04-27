97f7cab632d9df24860e7c7086d67dd2820bc0b1
Dodd Pfeffer <dpfeffer@pivotal.io>
2019-04-27 07:46:59 -0400

    Renames cloud-config.yml to take advantage of vs code's plugin

--- scripts/configure-bosh.sh
+++ scripts/configure-bosh.sh
@@ -1,4 +1,4 @@
 source scripts/bosh-env.sh
 
 bosh -e bosh-concourse-aws log-in
-bosh -e bosh-concourse-aws update-cloud-config bosh/cloud.yml -n
+bosh -e bosh-concourse-aws update-cloud-config bosh/cloud-config.yml -n
